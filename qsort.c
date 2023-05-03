4dbd8f32fbc76ed84dc5bb462339e24fbbabdeb9
guan99716 <101492695+guan99716@users.noreply.github.com>
2023-05-03 16:51:50 +0800

    Add files via upload

---
+++ qsort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+void swap(int[], int, int);
+void qsort2(int[], int, int);
+
+int main(void) {
+  srand(time(NULL));
+  int arr[1000];
+  for (int i = 0; i < 1000; i++)
+    arr[i] = rand() % 100 + 1; // 1~100
+
+  qsort2(arr, 0, 999);
+
+  for (int i = 0; i < 1000; i++)
+    printf("%d\n", arr[i]);
+
+  return 0;
+}
+
+void swap(int arr[], int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+void qsort2(int arr[], int left, int right) {
+  int i, j, pivot;
+  if (left < right) {
+    i = left;
+    j = right+1;
+    pivot = arr[left];
+    do {
+      do {i++;} while (arr[i] < pivot);
+      do {j--;} while (arr[j] > pivot);
+      if (i < j)
+        swap(arr, i, j);
+    } while (i < j);
+    swap(arr, left, j);
+    qsort2(arr, left, j - 1);
+    qsort2(arr, j + 1, right);
+  }
+}
